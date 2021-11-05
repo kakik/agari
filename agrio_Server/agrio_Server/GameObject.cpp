6e7b6d4b1e11486bb458baa31b3e35f51d416c7f
Ju yeong <namjuyeong21@gmail.com>
2021-11-05 23:02:41 +0900

    Update구현

---
+++ agrio_Server/agrio_Server/GameObject.cpp
@@ -0,0 +1,18 @@
+#include<cstring>
+#include "GameObject.h"
+#include"Protocol.h"
+
+void GameObject::Update(char* buf, int& bufStart) 
+{
+	sc_packet_obj_move pk;
+	pk.lookDir = direction;
+	pk.objectID = id;
+	pk.packetSize = sizeof(pk);
+	pk.packetType = SC_PACKET_OBJ_MOVE;
+	pk.x += 1;
+	pk.y += 1;
+	
+	memcpy(buf + bufStart, &pk, sizeof(pk));
+
+	bufStart += sizeof(pk);
+}
